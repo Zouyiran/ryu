9c9cb75c7176e745ff1a1d9505bf9a70bde1a41d
zouyiran <zouyiran9076@gmail.com>
2016-01-27 20:39:19 +0800

    installing flow was Done but ping error

--- ryu/app/demo/path_finder_3.py
+++ ryu/app/demo/path_finder_3.py
@@ -38,12 +38,19 @@ class PathFinder(app_manager.RyuApp):
     def __init__(self, *args, **kwargs):
         super(PathFinder, self).__init__(*args, **kwargs)
 
-
         self.mac_to_port = dict()
+
+        # {dpid:{mac:port,mac:port,...},dpid:{mac:port,mac:port,...},...}
         self.dpid_mac_to_port = dict()
+
         self.hostmac_to_dpid = dict()
         self.hostmac_to_port = dict()
+
+        #{(src_dpid,dst_dpid):(src_port,dst_port),():(),...}
         self.links_dpid_to_port = dict()
+
+        self.host_obj = list()
+        self.pre_host_obj = list()
 
         self.dpids = list()
         self.links = list()
@@ -67,6 +74,141 @@ class PathFinder(app_manager.RyuApp):
         self.SLEEP_PERIOD = 10 #seconds
         self.PRIORITY = OFP_DEFAULT_PRIORITY
 
+    def install_flows(self, traffic_table):
+        uri = "/stats/flowentry/add"
+        self.PRIORITY  += 1
+        for pair in traffic_table.keys():
+            src_host = pair[0] # src host mac
+            dst_host = pair[1] # dst host mac
+            in_port = None
+            out_port = None
+            traffic = traffic_table[pair] # [1,2,5]
+            traffic_len = len(traffic)
+            if traffic_len == 0: # unreachable
+                pass
+            elif traffic_len == 1:# belongs to a same dpid
+                dpid = traffic[0]
+                print("install_flows:",dpid)
+                dpid = traffic[0]
+                in_port = self.hostmac_to_port[src_host]
+                print("in_port:",in_port)
+                out_port = self.hostmac_to_port[dst_host]
+                print("out_port:",out_port)
+                data = self._pack_data(dpid, self.PRIORITY,in_port, out_port, src_host, dst_host)
+                requests.post(url=IP+uri,data=str(data))
+            # else: # several dpid
+            #     for i in range(traffic_len):
+            #         if i == 0:
+            #             in_port = self.hostmac_to_port[src_host]
+            #             path = (traffic[i],traffic[i+1])
+            #             if path in self.links_dpid_to_port.keys():
+            #                 out_port = self.links_dpid_to_port[path][0]
+            #         elif i == traffic_len-1:
+            #             out_port = self.hostmac_to_port[dst_host]
+            #             path = (traffic[i-1],traffic[i])
+            #             if path in self.links_dpid_to_port.keys():
+            #                 in_port = self.links_dpid_to_port[path][1]
+            #         else:
+            #             path_1 = (traffic[i-1],traffic[i])
+            #             if path_1 in self.links_dpid_to_port.keys():
+            #                 in_port = self.links_dpid_to_port[path_1][1]
+            #             path_2 = (traffic[i],traffic[i+1])
+            #             if path_2 in self.links_dpid_to_port.keys():
+            #                 out_port = self.links_dpid_to_port[path_2][0]
+            #         if in_port and out_port:
+            #             data = self._pack_data(traffic[i], self.PRIORITY,in_port, out_port, src_host, dst_host)
+            #             requests.post(url=IP+uri,data=str(data))
+
+    def _pack_data(self, dpid, priority, in_port, out_port, src_host, dst_host):
+        data = {"dpid":dpid,
+                "table_id":0,
+                "idle_timeout":100,
+                "hard_timeout":100,
+                "priority": 1111,
+                "match":{
+                    "in_port":in_port,
+                    "dl_dst":dst_host
+                },
+                "actions":[{"type":"OUTPUT",
+                            "port":out_port}]
+                }
+        return data
+
+    def discover_topology(self):
+        while True:
+            hub.sleep(self.SLEEP_PERIOD)
+            self.pre_adjacency_matrix = copy.deepcopy(self.adjacency_matrix)
+            # self.pre_host_obj = copy.deepcopy(self.host_obj)
+            self._update_topology()
+            self._update_hosts()
+            if self.pre_adjacency_matrix != self.adjacency_matrix:
+                self.logger.info('discover_topology thread: TOPO or HOSTS update...')
+                # self._show_matrix()
+                self.path_table = self._get_path_table(self.adjacency_matrix)
+                # self._show_path_table()
+                self.traffic_table = self._get_traffic_table(self.path_table)
+                # self._show_traffic_table()
+                self.install_flows(self.traffic_table)
+            # elif self.pre_adjacency_matrix != self.adjacency_matrix:
+            #     self.logger.info('discover_topology thread: TOPO  update...')
+            #     self._show_matrix()
+            #     self.path_table = self._get_path_table(self.adjacency_matrix)
+            #     self._show_path_table()
+            #     self.traffic_table = self._get_traffic_table(self.path_table)
+            #     self._show_traffic_table()
+            #     self.install_flows(self.traffic_table)
+            # elif self.pre_host_obj != self.host_obj:
+            #     self.logger.info('discover_topology thread: HOSTS update...')
+            #     self.traffic_table = self._get_traffic_table(self.path_table)
+            #     self._show_traffic_table()
+            #     self.install_flows(self.traffic_table)
+            # else:
+            #     self.logger.info('discover_topology thread: TOPO and HOSTS NOT update...')
+
+    def _update_topology(self):
+        switch_list = get_all_switch(self)
+        if switch_list:
+            self.dpid_mac_to_port = self._get_switches_mac_to_port(switch_list)
+            self.dpids = self._get_switches(switch_list) # dpid
+            # print(self.dpids)
+        link_dict = get_all_link(self)
+        if link_dict:
+            self.links_dpid_to_port = self._get_links_dpid_to_port(link_dict)
+            self.links = self._get_links(self.links_dpid_to_port) #(src.dpid,dst.dpid)
+        if self.dpids and self.links:
+            self.adjacency_matrix = self._get_adjacency_matrix(self.dpids, self.links)
+
+    def _update_hosts(self):
+        self.host_obj  = get_all_host(self)
+        if self.host_obj:
+            self.hostmac_to_dpid, self.hostmac_to_port = self._get_hosts_to_dpid_and_port(self.host_obj)
+            self.hosts = self._get_hosts(self.host_obj) # mac
+            # print(self.hosts)
+
+    def _get_path_table(self, matrix):
+        if matrix:
+            g = nx.Graph()
+            g.add_nodes_from(self.dpids)
+            for i in self.dpids:
+                for j in self.dpids:
+                    if matrix[i][j] == 1:
+                        g.add_edge(i,j,weight=1)
+            return self.__graph_to_path(g)
+
+    def __graph_to_path(self,g):
+        all_shortest_paths = dict()
+        for i in g.nodes():
+            for j in g.nodes():
+                if i == j:
+                    continue
+                all_shortest_paths[(i,j)] = list()
+                for each in nx.all_shortest_paths(g,i,j):
+                    try:
+                        all_shortest_paths[(i,j)].append(each)
+                    except nx.NetworkXNoPath:
+                        print("CATCH EXCEPTION: nx.NetworkXNoPath")
+        return all_shortest_paths
+
     def _get_traffic_table(self, path_table):
         traffic_table = dict()
         for src_host in self.hosts: # mac
@@ -79,89 +221,10 @@ class PathFinder(app_manager.RyuApp):
                     traffic_table[(src_host, dst_host)]  = [src_dpid]
                 elif (src_dpid, dst_dpid) in path_table.keys():
                     traffic_table[(src_host, dst_host)]  = path_table[(src_dpid, dst_dpid)][0]
+                else: # unreachable
+                    traffic_table[(src_host, dst_host)]  = []
         return traffic_table
 
-    def install_flows(self, traffic_table):
-        uri = "/stats/flowentry/add"
-        self.PRIORITY  += 1
-        for traffic in traffic_table.keys():
-            if len(traffic_table[traffic]) == 1:# belongs to a same dpid
-                dpid = traffic_table[traffic][0]
-                in_port = self.hostmac_to_port[traffic[0]]
-                print("in_port:",in_port)
-                out_port = self.hostmac_to_port[traffic[1]]
-                print("out_port:",out_port)
-                data = {"dpid":dpid,
-                        "table_id":0,
-                        "idle_timeout":100,
-                        "hard_timeout":100,
-                        "priority":11111,
-                        "match":{
-                            "in_port":in_port
-                        },
-                        "actions":[{"type":"OUTPUT",
-                                    "port":out_port}]
-                        }
-                json_data = json.dumps(data)
-                requests.post(url=IP+uri,data =str(data))
-            else:
-                pass
-
-    def discover_topology(self):
-        while True:
-            hub.sleep(self.SLEEP_PERIOD)
-            self.pre_adjacency_matrix = copy.deepcopy(self.adjacency_matrix)
-            self._update_topology()
-            if self.pre_adjacency_matrix != self.adjacency_matrix:
-                self.logger.info('discover_topology thread: topo update...')
-                self.path_table = self._get_path_table()
-                self.traffic_table = self._get_traffic_table(self.path_table)
-                self.install_flows(self.traffic_table)
-            else:
-                self.logger.info('discover_topology thread: topo NOT update...')
-
-    def _update_topology(self):
-        switch_list = get_all_switch(self)
-        if switch_list:
-            self.dpid_mac_to_port = self._get_switches_mac_to_port(switch_list)
-            self.dpids = self._get_switches(switch_list) # dpid
-            # print(self.dpids)
-
-        host_list = get_all_host(self)
-        if host_list:
-            self.hostmac_to_dpid, self.hostmac_to_port = self._get_hosts_to_dpid_and_port(host_list)
-            self.hosts = self._get_hosts(host_list) # mac
-            # print(self.hosts)
-
-        link_dict = get_all_link(self)
-        if link_dict:
-            self.links_dpid_to_port = self._get_links_dpid_to_port(link_dict)
-            self.links = self._get_links(self.links_dpid_to_port) #(src.dpid,dst.dpid)
-
-        if self.dpids and self.links:
-            self.adjacency_matrix = self._get_adjacency_matrix(self.dpids, self.links)
-            self._show()
-
-    def _get_path_table(self):
-        if self.adjacency_matrix:
-            g = nx.Graph()
-            g.add_nodes_from(self.dpids)
-            for i in self.dpids:
-                for j in self.dpids:
-                    if self.adjacency_matrix[i][j] == 1:
-                        g.add_edge(i,j,weight=1)
-            all_shortest_paths = dict()
-            for i in g.nodes():
-                for j in g.nodes():
-                    if i == j:
-                        continue
-                    all_shortest_paths[(i,j)] = list()
-                    for each in nx.all_shortest_paths(g,i,j):
-                        try:
-                            all_shortest_paths[(i,j)].append(each)
-                        except nx.NetworkXNoPath:
-                            print("CATCH EXCEPTION: nx.NetworkXNoPath")
-            return all_shortest_paths
 
     def _get_differ_path_table(self):
         # differ_path_table = list()
@@ -203,10 +266,7 @@ class PathFinder(app_manager.RyuApp):
                 self.logger.info('un register datapath: %04x', datapath.id)
                 del self.id_to_dp[datapath.id]
 
-
-
-
-    def _show(self):
+    def _show_matrix(self):
         switch_num = len(self.adjacency_matrix)
         print "---------------------adjacency_matrix---------------------"
         print '%10s' % ("switch"),
@@ -218,6 +278,20 @@ class PathFinder(app_manager.RyuApp):
             for j in self.adjacency_matrix[i].values():
                 print '%10.0f' % j,
             print ""
+
+    def _show_path_table(self):
+        print "---------------------path_table---------------------"
+        for pair in self.path_table.keys():
+            print("pair:",pair)
+            for each in self.path_table[pair]:
+                print each,
+            print""
+
+    def _show_traffic_table(self):
+        print "---------------------traffic_table---------------------"
+        for pair in self.traffic_table.keys():
+            print("pair:",pair)
+            print self.traffic_table[pair]
 
     def _get_switches_mac_to_port(self,switch_list):
         table = dict()
@@ -248,7 +322,6 @@ class PathFinder(app_manager.RyuApp):
         return link_ports_table.keys() #[(src.dpid,dst.dpid),(src.dpid,dst.dpid),...]
 
     def _get_hosts_to_dpid_and_port(self,host_list):
-        dpid_to_hostmac_to_port = dict()
         hostmac_to_dpid = dict()
         hostmac_to_port = dict()
         for host in host_list:
@@ -257,8 +330,6 @@ class PathFinder(app_manager.RyuApp):
             hostmac_to_port[host_mac] = host_port.port_no
             dpid = host_port.dpid
             hostmac_to_dpid[host_mac] = dpid
-            # dpid_to_host_to_port.setdefault(dpid,{})
-            # dpid_to_host_to_port[dpid][host_mac] = host_port.port_no
         return  hostmac_to_dpid, hostmac_to_port
 
     def _get_hosts(self, host_list):
